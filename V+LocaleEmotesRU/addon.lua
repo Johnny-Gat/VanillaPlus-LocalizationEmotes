a8375d75cee73994d6972c16f62135c8e22f131f
Johnny Gat <55049724+Johnny-Gat@users.noreply.github.com>
2022-02-02 17:38:16 +0300

    Добавлена 4 эмоция.

--- V+LocaleEmotesRU/addon.lua
+++ V+LocaleEmotesRU/addon.lua
@@ -1,29 +1,34 @@
 local DB = {
-	{e="applaud",
+	{e="applaud", -- applaud
 		{k="^You applaud.  Bravo!$",v="Вы аплодируете. Браво!"},
 		{k="^You applaud at (.+).  Bravo!$",v="Вы рукоплещете %s. Браво!"},
 		{k="^(.+) applauds.  Bravo!$",v="%s хлопает в ладоши. Браво!"},
 		{k="^(.+) applauds at you.  Bravo!$",v="%s рукоплещет вам. Браво!"},
 		{k="^(.+) applauds at (.+).  Bravo!$",v="%s рукоплещет %s. Браво!"}},
-	{e="beg",
+	{e="beg", -- beg
 		{k="^You beg everyone around you. How pathetic.$",v="Вы с мольбой смотрите на окружающих. Выглядит жалко."},
 		{k="^You beg (.+).  How pathetic.$",v="Вы с мольбой смотрите на %s. Выглядит жалко."},
 		{k="^(.+) begs everyone around him.  How pathetic.$",v="%s умоляет всех. Жалкое зрелище!"},
 		{k="^(.+) begs you.  How pathetic.$",v="%s с мольбой смотрит на вас. Выглядит жалко."},
 		{k="^(.+) begs (.+).  How pathetic.$",v="%s умоляет %s. Ничтожество!"}},
-	{e="bow",
+	{e="bow", -- bow
 		{k="^You bow down graciously.$",v="Вы изящно кланяетесь."},
 		{k="^You bow before (.+).$",v="Вы кланяетесь %s."},
 		{k="^(.+) bows down graciously.$",v="%s изящно кланяется."},
 		{k="^(.+) bows before you.$",v="%s кланяется вам."},
 		{k="^(.+) bows before (.+).$",v="%s кланяется %s."}},
+	{e="arms", -- chicken
+		{k="^With arms flapping, you strut around.  Cluck, Cluck, Chicken!$",v="Хлопая руками, вы расхаживаете вокруг. Цыпа-цыпа-цыпа!"},
+		{k="^With arms flapping, you strut around (.+).  Cluck, Cluck, Chicken!$",v="Хлопая руками, вы расхаживаете вокруг %s. Цыпа-цыпа-цыпа!"},
+		{k="^With arms flapping, (.+) struts around.  Cluck, Cluck, Chicken!$",v="Хлопая руками, %s расхаживает вокруг. Цыпа-цыпа-цыпа!"},
+		{k="^With arms flapping, (.+) struts around you.  Cluck, Cluck, Chicken!$",v="Хлопая руками, %s расхаживает вокруг вас. Цыпа-цыпа-цыпа!"},
+		{k="^With arms flapping, (.+) struts around (.+).  Cluck, Cluck, Chicken!$",v="Хлопая руками, %s расхаживает вокруг %s. Цыпа-цыпа-цыпа!"}},
 	{e="",
 		{k="^$",v=""},
 		{k="^$",v=""},
 		{k="^$",v=""},
 		{k="^$",v=""},
-		{k="^$",v=""}},	
-
+		{k="^$",v=""}},
 }
 
 local r
